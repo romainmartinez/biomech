bf2c10f0c3f80b542195db92e8778021e22f1c30
Romain Martinez <martinez.staps@gmail.com>
2017-08-23 18:27:42 -0400

    load project

--- +bmch/+preprocessing/configuration_files.m
+++ +bmch/+preprocessing/configuration_files.m
@@ -1,42 +1,49 @@
 function configuration_files(self)
 % create configurations files.
 % this is the first (necessary) step.
+
 clc
 self.ui.print_header
 fprintf('%s...\n', self.ui.category{str2double(self.field)})
 
 fileID = {'emg', 'markers', 'participants'};
 
-conf.folder = uigetdir(self.ui.category{str2double(self.field)});
+folder = uigetdir(self.ui.category{str2double(self.field)});
 
 switch self.field
     case '1' % create new bmch project
+        % display warning
         self.ui.display_warning(self.current)
-%         conf.folder = uigetdir(self.ui.category{str2double(self.field)});
         
-        mkdir(conf.folder, 'inputs'); % TODO: if no c3d, txt file with the data path
-        mkdir(conf.folder, 'outputs');
-        mkdir(conf.folder, 'conf');
+        % create folders
+        mkdir(folder, 'inputs'); % TODO: if no c3d, txt file with the data path
+        mkdir(folder, 'outputs');
+        mkdir(folder, 'conf');
         
         % create conf files (comma separated csv)
-        cellfun(@(x) bmch.util.cell2csv(sprintf('%s/conf/%s.csv', conf.folder, x), bmch.util.header(x)), fileID);
-                
-        answer = input('After filling the conf file, you can load the bmch project.\n [o]k?: ', 's');
+        cellfun(@(x) bmch.util.cell2csv(sprintf('%s/conf/%s.csv', folder, x), bmch.util.header(x)), fileID);
         
-        if answer ~= 'o'
-            error('please, type [o] or re-create a project [bmch warning].')
-        end
+        % display confirmation message
+        bmch.util.confirmation('createproject')
         
     case '2' % load existing bmch project
-        cellfun(@(x) bmch.util.cell2csv(sprintf('%s/conf/%s.csv', conf.folder, x), bmch.util.header(x)), fileID);
-        xi = {'emg', 'participants'}
-        xi = cellfun(@(x) readtable(sprintf('%s/conf/%s.csv', conf.folder, x)), xi);
+        % load csv conf files ('emg', 'markers', 'participants')
+        conf_files = cellfun(@(x) readtable(sprintf('%s/conf/%s.csv', folder, x)), fileID, 'UniformOutput', false);
         
-        xi = readtable(sprintf('%s/conf/%s.csv', conf.folder, xi));
+        % convert to conf.mat file
+        conf = cell2struct(conf_files, fileID, 2);
+        
+        % add current folder to conf file
+        conf.folder = folder;
+        
+        % add current folder to cache
+        save('./cache/cache.mat', 'folder')
+        
+        % save file to conf folder
+        save(sprintf('%s/conf/conf.mat', folder), 'conf');
+        
     otherwise
+        error('invalid argument [bmch warning].')
 end
 
-% conf path
-% conf participant
-% load existing folder
 end
