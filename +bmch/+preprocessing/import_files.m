c3998ef14596c222f48c71ae068fe16e28d9bacd
Romain Martinez <martinez.staps@gmail.com>
2017-08-24 17:27:30 -0400

    open c3d file

--- +bmch/+preprocessing/import_files.m
+++ +bmch/+preprocessing/import_files.m
@@ -2,10 +2,12 @@ classdef import_files
     % main launcher of the biomech toolbox.
     
     properties
-        main
-        dataroot
-        datadir
-        participants
+        main            % main class
+        fileio          % fileIO class
+        dataroot        % path to data
+        datadir         % folders contening data
+        participants    % participants name
+        current         % current field (emg, force, etc.)
     end % properties
     
     %-------------------------------------------------------------------------%
@@ -15,11 +17,21 @@ classdef import_files
             self.main = main;
             bmch.util.warnings('import_files')
             
+            % current field (emg, force, etc.)
+            self.current = main.ui.category(str2double(main.field));
+            
             % select participants
             self.participants = bmch.util.selector(main.conf.participants.pseudo);
-
+            
             % get folder path
-            self.dataroot = self.get_dataroot;   
+            self.dataroot = self.get_dataroot;
+            
+            % check if there is a folder for each selected participants
+            self.datadir = self.get_datadir(self.dataroot);
+            
+            % open data files
+            self.open_c3d
+            
         end % constructor
         
         %-------------------------------------------------------------------------%
@@ -36,6 +48,23 @@ classdef import_files
                 % data folder is in 'inputs', in the bmch project folder
                 dataroot = sprintf('%s/inputs/', self.main.conf.folder);
             end
+        end
+        
+        function datadir = get_datadir(~, dir_name)
+            %outputs a cell with directory names (as strings), given a certain dir name (string)
+            dd = dir(dir_name);
+            isub = [dd(:).isdir];
+            datadir = {dd(isub).name}';
+            datadir(ismember(datadir,{'.','..'})) = [];
+        end
+        
+        function open_c3d(self)
+            % get all data folders
+            folders = cellfun(@(x) sprintf('%s%s', self.dataroot, x), self.datadir, 'UniformOutput', false);
+            % fileIO class object
+            self.fileio = bmch.util.fileIO(self.current, folders);
+            % open c3d files
+            self.fileio.openc3d
         end
         
     end % methods
--- +bmch/+util/fileIO.m
+++ +bmch/+util/fileIO.m
@@ -2,16 +2,47 @@ classdef fileIO
     % main launcher of the biomech toolbox.
     
     properties
+        current         % current field (emg, force, etc.)
+        folders          % folder containing c3d files
     end % properties
     
     %-------------------------------------------------------------------------%
     methods
         
-        function self = fileIO(main)
+        function self = fileIO(current, folders)
+            self.current = current;
+            self.folders = folders;
         end % constructor
         
         %-------------------------------------------------------------------------%
-        function coucou
+        function openc3d(self)
+            for ifolder = self.folders'
+                filenames = dir(sprintf('%s/*.c3d', ifolder{:}));
+                for itrial = {filenames.name}
+                    % open btk object
+                    c = btkReadAcquisition(sprintf('%s/%s', ifolder{:}, itrial{:}));
+                    
+                    if contains(self.current, 'emg') || contains(self.current, 'force')
+                        d = btkGetAnalogs(c);
+                    elseif contains(self.current, 'markers')
+                        d = btkGetMarkers(c);
+                    end
+                  
+                    %A METTRE DANS FUNCTION%
+                    structname = fieldnames(d);
+                    %%%
+                    
+                    % close btk object
+                    btkCloseAcquisition(c);
+                end
+            end
+            %             c = btkReadAcquisition()
+            
+            
+            %               preallocation
+        end
+        
+        function assignc3dfields
         end
         
     end % methods
