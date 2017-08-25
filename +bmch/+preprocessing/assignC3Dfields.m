a8303766a6787a1345574f2a5b1dff067b8d8a2a
Romain Martinez <martinez.staps@gmail.com>
2017-08-25 16:57:24 -0400

    uiwait

--- +bmch/+preprocessing/assignC3Dfields.m
+++ +bmch/+preprocessing/assignC3Dfields.m
@@ -1,4 +1,4 @@
-classdef assignC3Dfields
+classdef assignC3Dfields < handle
     % GUI designed to assign c3d fields
     
     properties
@@ -6,6 +6,8 @@ classdef assignC3Dfields
         conf   % target channel names
         trial  % current trial
         gui    % gui data
+        output % correted fieldnames
+        idx    % current position (relative to conf number)
     end
     
     methods
@@ -13,25 +15,28 @@ classdef assignC3Dfields
             self.fields = fields;
             self.conf = conf;
             self.trial = trial;
+            self.output = {};
+            self.idx = 1;
             
             % check if conf == fields
             tocheck = ~ismember(self.conf, self.fields);
             
             if tocheck
                 self.gui = self.createInterface;
+                uiwait(self.gui.f)
             end
             
         end % constructor
         
         function gui = createInterface(self)
             % root figure
-            f = figure('units', 'normalized',...
+            gui.f = figure('units', 'normalized',...
                 'outerposition', [.25 .25 .5 .8],...
                 'MenuBar', 'none',...
                 'Toolbar', 'none');
             
             % panel
-            panel = uix.BoxPanel('Parent', f,...
+            panel = uix.BoxPanel('Parent', gui.f,...
                 'Title', 'channels assignment');
             
             % hbox
@@ -54,7 +59,7 @@ classdef assignC3Dfields
             
             gui.target = uicontrol('Parent', box1,...,
                 'Style', 'text',...
-                'String', 'current target');
+                'String', self.conf{self.idx});
             
             uicontrol('Parent', box1,...
                 'String', 'OK',...
@@ -78,25 +83,36 @@ classdef assignC3Dfields
             % 3) element 3: current assignments
             gui.conf = uicontrol('Parent', hbox,...
                 'Style', 'listbox',...
-                'String', self.conf);
+                'String', self.conf,...
+                'Value', self.idx);
             hbox.Widths = [-2 -1 -1 -1];
         end % createInterface
-         
-        function self = updateInterface(self, src, ~)
-            if contains(src.String, 'NaN')
-                disp('NaN')
-            else
-                disp('OK')
-            end
+        
+        function updateInterface(self, src, ~)
+            self.idx = self.idx + 1;
             % get current selection
             current_field = self.fields(self.gui.fields.Value);
             
+            % delete current selection from gui
             self.fields(contains(self.fields, current_field)) = [];
             
-            self.gui.fields.String = self.fields;
+            % add field in the output
+            if contains(src.String, 'NaN')
+                self.output = [self.output nan];
+            else
+                self.output = [self.output current_field];
+            end
+            
+            % update gui
+            if self.idx > length(self.conf)
+                close all
+            else
+                self.gui.fields.String = self.fields;
+                self.gui.target.String = self.conf{self.idx};
+                self.gui.conf.Value = self.idx;
+            end
         end % nan
         
     end
     
 end
-
